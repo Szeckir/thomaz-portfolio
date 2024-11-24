ea651836cd29117cb96b15a48b071ce5932d17f7
Thomaz Szeckir <thomazszeckir@Thomazs-Air.lan>
2024-11-24 15:52:42 -0500

    added contact page

--- src/components/ux/Contact.tsx
+++ src/components/ux/Contact.tsx
@@ -1,7 +1,23 @@
 function Contact() {
   return (
-    <div className="mt-12 justify-center text-center items-center">
-        <h1>CONTAACCTT</h1>
+    <div className="mt-12">
+      <div className="flex gap-8 justify-center">
+        <div className="bg-white w-16 h-16 rounded-full">
+
+        </div>
+
+        <div className="bg-white w-16 h-16 rounded-full">
+
+        </div>
+
+        <div className="bg-white w-16 h-16 rounded-full">
+
+        </div>
+      </div>
+
+      <div className="justify-center flex mt-4">
+        <div className="bg-white w-64 h-64 rounded-full"></div>
+      </div>
     </div>
   )
 }
